7129ca11c405ff2390e56844105d35b084e25fc0
davidluupli <oluyaled@gmail.com>
2025-01-11 14:17:44 +0100

    referrer

--- src/Routes/emailRoutes.js
+++ src/Routes/emailRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import AlertCaregiversAboutNewJob from "../Emails/Caregivers/AlertCaregiversAboutNewJob.js";
 import sendCaregiversAlert from "../Emails/Providers/AlertProvidersAboutNewCandidate.js";
 import supportResources from "../Emails/Campigns/caregiver_training_support_resources.js";
-import { inviteFriends, referToEmployer } from "../Emails/Caregivers/CareerProfileEmail.js";
+import { inviteFriends, referToEmployer, invitedEmployerEmail } from "../Emails/Caregivers/CareerProfileEmail.js";
 const router = Router()
 
 // caregivers 
@@ -13,6 +13,8 @@ router.post('/send-new-caregivers-alert', sendCaregiversAlert)
 
 router.post("/invite-friend", inviteFriends)
 router.post('/refer-employer', referToEmployer)
+router.post('/invited-employer-email', invitedEmployerEmail)
+
 
 router.post('/support-resources', supportResources)
 export default router
